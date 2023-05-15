57a0fe89eaa02b0ad79081ed80799b8823297a79
Rahul-s-007 <rahulrtg7703@gmail.com>
2023-05-16 01:33:18 +0400

    small changes

--- Index Creation/create_index.py
+++ Index Creation/create_index.py
@@ -1,7 +1,6 @@
 #------------------------------------
 # pytesseract
 # pdf2image
-
 import openai
 import pinecone
 
@@ -20,73 +19,70 @@ pinecone.init(
     environment=PINECONE_ENVIRONMENT_NAME  # next to api key in console
 )
 
-#------------------------------------
-import os
-arr = os.listdir()
-arr = os.listdir("dataset")
-
-index_name = []
-
-for i in range(len(arr)):
-    index_name.append((arr[i].replace(" ", "-").replace(".txt", "").lower()))
-
-for i in range(len(arr)):
-    arr[i] = "dataset/" + arr[i]
-
+# Create Index for Customer service representative interview data
 from langchain.document_loaders import UnstructuredFileLoader
+
+loader = UnstructuredFileLoader("dataset\Customer service representative.txt")
+pages = loader.load_and_split()
+
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
-embeddings = OpenAIEmbeddings()
+
 text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size = 1000,
-        chunk_overlap  = 200,
-        length_function = len,
-    )
+    chunk_size = 1000,
+    chunk_overlap  = 200,
+    length_function = len,
+)
 
+docs = text_splitter.split_documents(pages)
 
-for i in range(1,len(arr)):
-    loader = UnstructuredFileLoader(arr[i])
-    pages = loader.load_and_split()
+from langchain.embeddings.openai import OpenAIEmbeddings
 
-    docs = text_splitter.split_documents(pages)
+embeddings = OpenAIEmbeddings()
 
-    index_name = index_name[i]
-    # pinecone.create_index(index_name, dimension=1536, metric="cosine", pods=1)
-    # # create a new index
-    docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-    print("Index created:",index_name)
+from langchain.vectorstores import Pinecone
+
+index_name = "customer-service-representative"
+
+#create a new index
+docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+print("Index created:",index_name)
 
 #------------------------------------
+# Create Multiple Indexes
+# import os
+# arr = os.listdir()
+# arr = os.listdir("dataset")
+
+# index_name = []
+
 # for i in range(len(arr)):
-#     index_name.append(arr[i].replace(" ", "-").replace(".txt", ""))
+#     index_name.append((arr[i].replace(" ", "-").replace(".txt", "").lower()))
 
 # for i in range(len(arr)):
 #     arr[i] = "dataset/" + arr[i]
 
 # from langchain.document_loaders import UnstructuredFileLoader
-
-# loader = UnstructuredFileLoader("dataset\Customer service representative.txt")
-# pages = loader.load_and_split()
-
 # from langchain.text_splitter import RecursiveCharacterTextSplitter
-
-# text_splitter = RecursiveCharacterTextSplitter(
-#     chunk_size = 1000,
-#     chunk_overlap  = 200,
-#     length_function = len,
-# )
-
-# docs = text_splitter.split_documents(pages)
-
 # from langchain.embeddings.openai import OpenAIEmbeddings
-
-# embeddings = OpenAIEmbeddings()
-
 # from langchain.vectorstores import Pinecone
+# embeddings = OpenAIEmbeddings()
+# text_splitter = RecursiveCharacterTextSplitter(
+#         chunk_size = 1000,
+#         chunk_overlap  = 200,
+#         length_function = len,
+#     )
 
-# index_name = "customer-service-representative"
 
-# #create a new index
-# docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
-# print("Index created:",index_name)
+# for i in range(1,len(arr)):
+#     loader = UnstructuredFileLoader(arr[i])
+#     pages = loader.load_and_split()
+
+#     docs = text_splitter.split_documents(pages)
+
+#     index_name = index_name[i]
+#     # pinecone.create_index(index_name, dimension=1536, metric="cosine", pods=1)
+#     # # create a new index
+#     docsearch = Pinecone.from_documents(docs, embeddings, index_name=index_name)
+#     print("Index created:",index_name)
+
+#------------------------------------
