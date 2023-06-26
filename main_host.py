474cdf7768d3a548d15a5bb6b9d8df42cde0e931
Rahul-s-007 <rahulrtg7703@gmail.com>
2023-06-26 09:04:24 +0530

    host file

--- main_host.py
+++ main_host.py
@@ -20,6 +20,7 @@ pinecone.init(
     api_key=PINECONE_API_KEY,  # find at app.pinecone.io
     environment=PINECONE_ENVIRONMENT_NAME  # next to api key in console
 )
+
 # ------------------------------------------------------------------------------------
 from langchain.embeddings.openai import OpenAIEmbeddings
 embeddings = OpenAIEmbeddings()
@@ -33,18 +34,18 @@ llm = OpenAI(temperature=0)
 #----------------------------------------------------------
 from langchain.vectorstores import Pinecone
 
-index_name = "customer-service-representative"
+index_name = "customer-service-representative-index"
 docsearch = Pinecone.from_existing_index(index_name, embeddings)
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=docsearch.as_retriever())
 
 #----------------------------------------------------------
 if "asked_questions" not in st.session_state:
-    st.session_state["asked_questions"] = """Below are already asked questions, dont use them again:"""
+    st.session_state["asked_questions"] = []
 # asked_questions = """Below are already asked questions, dont use them again:"""
 
-if "prev_template" not in st.session_state:
-    st.session_state["prev_template"] = """Answer given by user for the above question is:"""
-# prev_template = """Answer given by user for the above question is:"""
+# if "prev_template" not in st.session_state:
+#     st.session_state["prev_template"] = []
+# # prev_template = """Answer given by user for the above question is:"""
 
 if "question_answer_pair" not in st.session_state:
     st.session_state["question_answer_pair"] = []
@@ -61,6 +62,8 @@ if "prev_ans" not in st.session_state:
 if "question" not in st.session_state:
     st.session_state["question"] = ""
 
+if "report" not in st.session_state:
+    st.session_state["report"] = """"""
 #----------------------------------------------------------
 if "past" not in st.session_state:
     st.session_state["past"] = []
@@ -73,39 +76,83 @@ if 'userans' not in st.session_state:
 #----------------------------------------------------------
 
 chat_col,temp = st.columns([10,1])
-col1,col2 = st.columns([1,1])
-text_col, temp = st.columns([10,1])
-report_col,temp = st.columns([10,1])
+col1,space,col2 = st.columns([2,1,1])
+text_col, temp = st.columns([11,1])
+report_col,temp = st.columns([11,1])
 
 #----------------------------------------------------------
 def get_suggestions(qa_pair):
-    report = "Below given is the question asked by you and the answer given by the user:\n"
-    report += f"Question:{qa_pair[0]}\nAnswer:{qa_pair[1]}\n"
-    report += "Now for the above qiven question and answer, give a score for the answer on a scale of 1 to 10. Also give suggestions on how to improve the user's answer."
-    
+    report = f"""
+Below is given the question asked by Interviewer delimited by a triple backtick (```) and the answer is delimited by a double backtick (``)
+Question:```{qa_pair[0]}```
+User Answer:``{qa_pair[1]}``
+
+Guidelines for rating the answer:
+0: User does not know answer the question.
+1-3: Poor answer, did not cover any key topic, needs a lot of improvement.
+3-5: Average answer, covered some key topics, needs some improvement.
+5-7: Good answer, covered most key topics, needs minor improvement.
+7-10: Excellent answer, covered all key topics, no improvement needed.
+
+Perform the following actions:
+1- Get a list of topics/key things a good answer for that question should have and use it in the later steps.
+2- Analyze the User's answer provided above and rate it on a scale of 1 to 10 and be srict while rating.
+3- Give suggestions on how the User can improve their answer.
+
+Give output in the following format:
+Rating: <Rating here>/10 \n\n
+
+Suggestions: <Suggestions here> \n\n
+
+Helpful Answer: <Helpful Answer Here> \n\n
+"""
+
     result_2 = qa({"query": report})
     suggestion_txt = result_2['result']
-    
+    # print(suggestion_txt)
     return suggestion_txt
 
 #----------------------------------------------------------
 def submit():
     st.session_state.userans = st.session_state.input
     st.session_state.input = ''
-    st.session_state["prev_ans"] = st.session_state["prev_template"] + "\n" + st.session_state.userans
+    st.session_state["prev_ans"] = st.session_state.userans
     st.session_state["question_answer_pair"].append([st.session_state["question"], st.session_state["userans"]])
     st.session_state["past"].append(st.session_state["userans"])
     st.session_state["suggestions"].append(get_suggestions(st.session_state["question_answer_pair"][-1]))
+    st.session_state["report"] = """"""
+    for i in range(len(st.session_state["suggestions"])):
+        st.session_state["report"] += f"Q.{i+1} {st.session_state['question_answer_pair'][i][0]} \n\nAns: {st.session_state['question_answer_pair'][i][1]} \n\nAnalysis:\n{st.session_state['suggestions'][i]}\n\n"
     # with a_col:
     #     st.write(st.session_state.userans)
 
 #----------------------------------------------------------
 def next_question():
-    query_template = f"""Give any one of the questions the Interviewer asked or cross question based on the users previous answer.
-Also if user does not know the answer of a certain question move on to next question: {st.session_state["asked_questions"]} {st.session_state["prev_ans"]}"""
-    
+    if len(st.session_state["past"])%2==1:
+        st.session_state["prev_ans"] = "No answer."
+        st.session_state["question_answer_pair"].append([st.session_state["question"], "No answer."])
+        st.session_state["past"].append("No answer.")
+        st.session_state["suggestions"].append(get_suggestions([st.session_state["question"], "No answer."]))
+        st.session_state["report"] = """"""
+        for i in range(len(st.session_state["suggestions"])):
+            st.session_state["report"] += f"Q.{i+1} {st.session_state['question_answer_pair'][i][0]} \n\nAns: {st.session_state['question_answer_pair'][i][1]} \n\nAnalysis:\n{st.session_state['suggestions'][i]}\n\n"
+
+    query_template = f"""
+Do one of the following:
+1. Give a new Question.
+2. Cross question based on the users previous answer.
+If user does not know the answer of a certain question move on to next question.
+Try to ask as many questions new questions as possible.
+Previously asked question are delimited by a triple backtick (```) and the answer to last asked question is delimited by a double backtick (``).
+Do not repeat the questions that are already asked.
+```{st.session_state["asked_questions"]}```
+``{st.session_state["prev_ans"]}``
+"""
+
     result_1 = qa({"query": query_template})
     question = result_1['result']
+    question = question.replace("\n","")
+    question = question.strip()
     return question
 
 #----------------------------------------------------------
@@ -113,7 +160,7 @@ def chat():
     with col1:
         if st.button("New Question"):
             st.session_state["question"] = next_question()
-            st.session_state["asked_questions"] += "\n" + st.session_state["question"]
+            st.session_state["asked_questions"].append(st.session_state["question"])
             st.session_state["past"].append(st.session_state["question"])
             
             # with q_col:
@@ -127,7 +174,7 @@ def chat():
             if(ind%2==0):
                 message(i, key=str(i))
             else:
-                message(i, is_user=True, key=str(i) + '_user')
+                message(i, is_user=True, key=str(i)+'_user')
             # with q_col:
             #     st.write(i[0])
             # with a_col:
@@ -140,7 +187,12 @@ def chat():
                 st.write("Report:")
                 for i in range(len(st.session_state["suggestions"])):
                     # print(f"Q.{i+1} {st.session_state['question_answer_pair'][i][0]}\nAns: {st.session_state['question_answer_pair'][i][1]}\nSuggestion:{st.session_state['suggestions'][i]}\n")
-                    st.write(f"Q.{i+1} {st.session_state['question_answer_pair'][i][0]} \n\n Ans: {st.session_state['question_answer_pair'][i][1]} \n\n Suggestion:{st.session_state['suggestions'][i]} \n\n")
+                    suggestion_lst = st.session_state['suggestions'][i].split("\n")
+                    suggestion = ""
+                    for j in suggestion_lst:
+                        if j != '':
+                            suggestion += j+"\n\n"
+                    st.write(f"Q.{i+1} {st.session_state['question_answer_pair'][i][0]} \n\n Ans: {st.session_state['question_answer_pair'][i][1]} \n\n Analysis:\n\n{suggestion} \n\n")
     with text_col:
         input_text = st.text_input("You: ", st.session_state["input"], key="input",
                                 placeholder="Enter your response here...", 
@@ -170,6 +222,8 @@ job_roles = ["Customer Service Representative"]
 
 #----------------------------------------------------------------#
 def main():
+    st.sidebar.image("doc_imgs/ai-chatbot-thumb.webp")
+    
     st.sidebar.write("Choose a role to be interviewed for:")
     options = st.sidebar.radio("Select Role",job_roles,label_visibility="collapsed")
     
@@ -177,16 +231,23 @@ def main():
         chat_customer_service_representative()
 
     if st.sidebar.button("Clear Chat"):
-        st.session_state["asked_questions"] = """Below are already asked questions, dont use them again:"""
-        st.session_state["prev_template"] = """Answer given by user for the above question is:"""
+        st.session_state["asked_questions"] = []
+        # st.session_state["prev_template"] = """Answer given by user for the above question is:"""
         st.session_state["question_answer_pair"] = [] 
         st.session_state["suggestions"] = []
         st.session_state["prev_ans"] = ""
         st.session_state["question"] = ""
         st.session_state["past"] = []
-        st.session_state.userans = ''
-        
+        st.session_state.userans = ""
+
+    if st.sidebar.download_button("Download Report", st.session_state["report"], file_name="report.txt"):
+        st.sidebar.success("Report downloaded successfully.")
+
     st.sidebar.success("Press 'New Question' to start the interview.")
+    
+    st.sidebar.write("Roles in the future:")
+    st.sidebar.write("Software Developer, Marketing Manager, Data Scientist, Human Resources Manager, Project Manager, Financial Analyst, Customer Service Representative, Graphic Designer, Healthcare Administrator, Lawyer, Teacher, Web Developer")
+
 #----------------------------------------------------------------#
 if __name__ == "__main__":
     main()
